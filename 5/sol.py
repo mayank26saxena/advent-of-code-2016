baa583353b00c2249b88b3436a5950a732f605db
Mayank Saxena <mayank26saxena@gmail.com>
2016-12-06 01:43:02 +0530

    day 5 part 2 solved

--- 5/sol.py
+++ 5/sol.py
@@ -1,13 +1,9 @@
 import hashlib
 
 door_id = 'wtnhxymk'
-
 index = 0
-
 count = 0
-
-password = ''
-
+word = 'ssssssss'
 zeros = '00000'
 
 while count < 8 :
@@ -16,14 +12,16 @@ while count < 8 :
 	hexa = hash_object.hexdigest()
 	first_five = hexa[0:5]
 	if first_five == zeros :
-		password += hexa[5]
-		count = count + 1
-		print 's = %s ' % s
+		position = hexa[5]
+		i = int(position, 16)
+		char = hexa[6]
+		if i < 8 :
+			print "Position %s i %d " % (position , i)
+			if word[i] == 's' :
+				count = count + 1
+				word = word[:i] + char + word[i + 1:]
+			print "Word is %s " % word
+			#print 's = %s ' % s
 	index = index + 1
-	
 
-#hash_object = hashlib.md5(door_id)
-#hexa = hash_object.hexdigest()
-#print hexa
-
-print "Password is %s " % password
+print "Password is %s " % word
